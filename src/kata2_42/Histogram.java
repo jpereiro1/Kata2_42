b52e9272a9454b91ede8adc83fcf7f653d0dd740
Entrar <Entrar@PC-1120>
2021-09-30 10:24:43 +0100

    Versión 3.Beta Kata 2 - Ingeniería Software 2 ULPGC

---
+++ src/kata2_42/Histogram.java
@@ -0,0 +1,12 @@
+package kata2_42;
+
+
+public class Histogram {
+    private final int[] data;
+
+    public Histogram() {
+        data = new int[]{2,2,1,3,5,6,7,2,4,6,2,6,9,2,4}; 
+    }
+    
+    
+}
